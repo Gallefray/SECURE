373e54814366419a32844578a9f93f63a0226766
James Game <james.game.64@gmail.com>
2014-07-25 02:20:24 +0200

    Defined roles and added irc.c
    Finn, I tried to base this on our conversation (things we agreed on), if I misunderstood something email/IRC me (?)

--- src/secure.h
+++ src/secure.h
@@ -14,16 +14,36 @@ typedef enum {false, true} bool;
 // main.c
 
 // cmd.c
-void cmd_getcmd(void);
-void cmd_parse(void);
+// Parses text input into commands
+void cmd_getcmd(void); //is this really needed?
+void cmd_parse(void); //seems to be doing well by itself
+//BTW, I belive the CLI should have a separate .h, let's keep this one clean (as API?)
 
 // pgp.c
+// Handles encryption/decryption
 void pgp_getmsg(void); // uses getline(void) to get the message line by line. Concats and stores it in a buffer (eventually a libgpgme buffer)
 void pgp_getpub(void);
 void pgp_verify(void);
 void pgp_decrypt(void);
 
 // io.c (Can later be altered for readline and sockets)
+// (please define)
 void io_getlin(void);
 void io_putlin(void);
+
+// irc.c
+// IRC functions
+void irc_conn(void);
+void irc_join(void);
+void irc_disconn(void);
+void irc_whois(void);
+void irc_msg(void);
+void irc_getmsg(void);
+void irc_send(void);
+void irc_get(void);
+//not quite complete
+
+// socket.c
+// This one's yours, Finn!
+
 #endif SECURE_HEADER
